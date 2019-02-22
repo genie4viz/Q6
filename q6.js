10bbcc24efa47b75d162d8ee2c18094219c0100a
selistym <selistym@protonmail.com>
2019-02-22 14:16:17 -0800

    completed

--- q6.js
+++ q6.js
@@ -7,9 +7,11 @@ var detail_map = d3.map();
 
 var path = d3.geoPath();
 
+var tip = d3.tip().attr('class', 'd3-tip').html(function(d) { return d; });
+
 var x = d3.scaleLinear()
     .domain([1, 10])
-    .rangeRound([600, 860]);
+    .rangeRound([100, 300]);
 
 var color = d3.scaleThreshold()
     .domain(d3.range(2, 10))
@@ -17,7 +19,7 @@ var color = d3.scaleThreshold()
 
 var g = svg.append("g")
     .attr("class", "key")
-    .attr("transform", "translate(0,40)");
+    .attr("transform", "translate(930,240)");
 
 g.selectAll("rect")
     .data(color.range().map(function (d) {
@@ -27,11 +29,11 @@ g.selectAll("rect")
         return d;
     }))
     .enter().append("rect")
-    .attr("height", 8)
-    .attr("x", function (d) {
+    .attr("width", 8)
+    .attr("y", function (d) {        
         return x(d[0]);
     })
-    .attr("width", function (d) {
+    .attr("height", function (d) {
         return x(d[1]) - x(d[0]);
     })
     .attr("fill", function (d) {
@@ -40,14 +42,14 @@ g.selectAll("rect")
 
 g.append("text")
     .attr("class", "caption")
-    .attr("x", x.range()[0])
-    .attr("y", -6)
+    .attr("x", -40)
+    .attr("y", 80)
     .attr("fill", "#000")
     .attr("text-anchor", "start")
     .attr("font-weight", "bold")
-    .text("poverty_map rate");
+    .text("Poverty map rate");
 
-g.call(d3.axisBottom(x)
+g.call(d3.axisLeft(x)
         .tickSize(13)
         .tickFormat(function (x, i) {
             return i ? x : x + "%";
@@ -58,31 +60,39 @@ g.call(d3.axisBottom(x)
 
 var promises = [
     d3.json("us.json"),
-    d3.csv("county_poverty.csv", function (d) {
-        // console.log(d);
-        poverty_map.set(d.id, +d.CensusId);
+    d3.csv("county_poverty.csv", function (d) {        
+        poverty_map.set(d.CensusId, +d.Poverty);
+        poverty_map.set(d.CensusId + "_state", d.State);
+        poverty_map.set(d.CensusId + "_county", d.County);
     }),
-    d3.csv("county_detail.csv", function (d) {        
-        detail_map.set(d.id, +d.CensusId);
+    d3.csv("county_detail.csv", function (d) {
+
+        detail_map.set(d.CensusId + "_total", +d.TotalPop);
+        detail_map.set(d.CensusId + "_income", +d.IncomePerCap);
     })
 ]
 
 Promise.all(promises).then(ready);
 
-function ready([us]) {
-    console.log([us]);
+function ready([us]) {    
     svg.append("g")
         .attr("class", "counties")
         .selectAll("path")
         .data(topojson.feature(us, us.objects.counties).features)
         .enter().append("path")
         .attr("fill", function (d) {
-            return color(d.rate = poverty_map.get(d.id));
+            d.Poverty = poverty_map.get(d.id);
+            d.State = poverty_map.get(d.id + "_state");
+            d.County = poverty_map.get(d.id + "_county");
+            d.TotalPop = detail_map.get(d.id + "_total");
+            d.IncomePerCap = detail_map.get(d.id + "_income");
+            return color(d.Poverty);
         })
         .attr("d", path)
-        .append("title")
-        .text(function (d) {
-            return d.rate + "%";
+        .call(tip)
+        .on('mouseover', function (d) {            
+            var strInner = d.State + `<br>` + d.County + `<br>` + d.Poverty + `%<br>` + d.TotalPop + `<br>` + d.IncomePerCap;
+            tip.show(strInner);
         });
 
     svg.append("path")
